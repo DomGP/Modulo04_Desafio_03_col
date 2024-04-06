3f52dc8e4614c8de2113b2bd9048979dad5d58ad
Alejandro <alejandroidb@gmail.com>
2024-04-06 18:03:13 -0300

    actualizacion componente alarma

--- src/components/ALerta.jsx
+++ src/components/ALerta.jsx
@@ -1,9 +1,15 @@
-const Alerta = () => {
-    return (
-        <>
-        
-        </>
-    )
-}
+import React from "react"; //importar react
 
-export default Alerta
+const Alerta = ({ message, success }) => {
+  //funcion para mostrar mensaje de alerta
+  const alertClass = success ? "alert-success" : "alert-danger"; //condicion para mostrar mensaje de aceptar o alerta de campos vacions
+
+  return (
+    //al ejecutar la funcion nos retornará un div con el mensaje correspondiente
+    <div className={`alert ${alertClass} mt-2`} role="alert">
+      {message}
+    </div>
+  );
+};
+
+export default Alerta; //exportamos para agregar en otros componentes
